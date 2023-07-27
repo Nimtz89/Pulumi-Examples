432da70b3de3d8aa47dea461773e33a6c85a9c36
Anco van den Berg <anco.vandenberg@winvision.nl>
2023-07-27 12:59:35 +0200

    Added function

---
+++ typescript-collection/Azure-Function/index.ts
@@ -0,0 +1,26 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("pulumiPoc", {
+    resourceGroupName: "pulumi-examples",
+});
+
+// Create an Azure resource (Storage Account)
+const storageAccount = new storage.StorageAccount("sa", {
+    resourceGroupName: resourceGroup.name,
+    accountName: "pulumiexamplesstorage",
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
+
+// Export the primary key of the Storage Account
+const storageAccountKeys = storage.listStorageAccountKeysOutput({
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name
+});
+
+export const primaryStorageKey = storageAccountKeys.keys[0].value;
